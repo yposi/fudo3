ce941b40250d2322ecb8b58ab5fcf30190ba050d
Shuhei Yamashita <y.shuhei0501@gmail.com>
2020-08-25 16:36:22 +0900

    平米・坪・畳の計算を追加

--- lib/fudo3.rb
+++ lib/fudo3.rb
@@ -2,5 +2,42 @@ require "fudo3/version"
 
 module Fudo3
   class Error < StandardError; end
-  # Your code goes here...
+
+  TSUBO_BASE = 3.305785
+  JO_BASE = 1.6562
+
+  # 平米 → 坪 変換
+  def self.heibei_to_tsubo(heibei)
+    heibei / TSUBO_BASE
+  end
+
+  # 平米 → 畳 変換
+  def self.heibei_to_jo(heibei)
+    heibei / JO_BASE
+  end
+
+  # 坪 → 平米 変換
+  def self.tsubo_to_heibei(tsubo)
+    tsubo * TSUBO_BASE
+  end
+
+  # 坪 → 畳 変換
+  def self.tsubo_to_jo(tsubo)
+    tsubo * TSUBO_BASE / JO_BASE
+  end
+
+  # 畳 → 平米 変換
+  def self.jo_to_heibei(jo)
+    jo * JO_BASE
+  end
+
+  # 畳 → 坪 変換
+  def self.jo_to_tsubo(jo)
+    jo * JO_BASE / TSUBO_BASE
+  end
+
+  # 平米
+  def self.heibei(width, height)
+    width * height
+  end
 end
--- spec/fudo3_spec.rb
+++ spec/fudo3_spec.rb
@@ -1,9 +1,25 @@
 RSpec.describe Fudo3 do
-  it "has a version number" do
-    expect(Fudo3::VERSION).not_to be nil
+  describe '.heibei_to_tsubo' do
+    it { expect(Fudo3.heibei_to_tsubo(1)).to eq 0.30250001134375043 }
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.heibei_to_jo' do
+    it { expect(Fudo3.heibei_to_jo(1)).to eq 0.6037918125830214 }
+  end
+
+  describe '.tsubo_to_heibei' do
+    it { expect(Fudo3.tsubo_to_heibei(1)).to eq 3.305785 }
+  end
+
+  describe '.tsubo_to_jo' do
+    it { expect(Fudo3.tsubo_to_jo(1)).to eq 1.9960059171597635 }
+  end
+
+  describe '.jo_to_heibei' do
+    it { expect(Fudo3.jo_to_heibei(1)).to eq 1.6562 }
+  end
+
+  describe '.jo_to_tsubo' do
+    it { expect(Fudo3.jo_to_tsubo(1)).to eq 0.5010005187875194 }
   end
 end
